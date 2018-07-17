d168337681f6f1428eb9c36b0fe0f023b51fb86b
Gabriel Ng <gabrielng15@gmail.com>
2018-07-17 13:40:47 -0700

    added sections to homepage

--- public/scripts/homepage.js
+++ public/scripts/homepage.js
@@ -3,3 +3,13 @@ $(document).ready(function() {
     $("#sidebar").toggleClass("active");
   });
 });
+
+function openNav() {
+  document.getElementById("hamburgerNav").style.width = "250px";
+  document.getElementById("page").style.marginRight = "250px";
+}
+
+function closeNav() {
+  document.getElementById("hamburgerNav").style.width = "0";
+  document.getElementById("page").style.marginRight = "0";
+}
