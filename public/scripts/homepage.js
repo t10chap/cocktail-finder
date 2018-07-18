994f205d42793908c970cb409e68f79235336eed
Gabriel Ng <gabrielng15@gmail.com>
2018-07-18 10:23:05 -0700

    added ajax call for displaying drinks

--- public/scripts/homepage.js
+++ public/scripts/homepage.js
@@ -26,10 +26,28 @@ $(document).ready(function() {
   let randomSearchUrl =
     "https://www.thecocktaildb.com/api/json/v1/1/random.php";
 
+  const displaySearch = response => {};
+
+  const displayError = (err1, err2, err3) => {};
+
   $("input[type='submit']").on("click", function(e) {
     e.preventDefault();
+    let url = "";
     let currentSearchSelection = $(".form-control").val();
     let userSearch = $("input[name='search']").val();
-    $.ajax({});
+    if (currentSearchSelection == "name") {
+      url = searchByNameUrl + userSearch;
+    } else if (currentSearchSelection == "liquor") {
+      url = searchByIngredientUrl + userSearch;
+    } else if (currentSearchSelection == "random") {
+      url = randomSearchUrl;
+    }
+
+    $.ajax({
+      method: "GET",
+      url: url,
+      success: displaySearch,
+      error: displayError
+    });
   });
 });
