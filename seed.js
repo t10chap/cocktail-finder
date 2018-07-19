fe94ee78497df84a83833990eae4ca0f4d99d8bd
Gabriel Ng <gabrielng15@gmail.com>
2018-07-18 23:13:11 -0700

    added user db initializers in seed.js

--- seed.js
+++ seed.js
@@ -2,6 +2,16 @@ let db = require("./models");
 
 let drinkList = [];
 
+let userList = [
+  {
+    name: Test,
+    location: 92110,
+    favoriteLiquor: Tequila,
+    favoriteDrink: Patron,
+    savedDrinks: []
+  }
+];
+
 db.Drink.remove({}, (err, drinks) => {
   db.Drink.create(drinkList, (err, drinks) => {
     if (err) {
@@ -9,6 +19,17 @@ db.Drink.remove({}, (err, drinks) => {
     }
     console.log("All drinks: ", drinks);
     console.log("Created ", drinks.length, " drinks");
+    process.exit();
+  });
+});
+
+db.User.remove({}, (err, users) => {
+  db.Drink.create(userList, (err, users) => {
+    if (err) {
+      return console.log("ERROR", err);
+    }
+    console.log("All users: ", users);
+    console.log("Created ", users.length, " users");
     process.exit();
   });
 });
