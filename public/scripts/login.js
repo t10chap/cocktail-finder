707a37113a9effbcbb3aeee609a1aecb8b67b441
Gabriel Ng <gabrielng15@gmail.com>
2018-07-19 16:11:33 -0700

    fixed merge error

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -5,37 +5,35 @@ $(document).ready(function() {
     $(".formContainer").fadeIn(900);
   });
 
-// Sign in
-  $(".signin").click(function(){
+  // Sign in
+  $(".signin").click(function() {
     let username = $("#inputUser").val();
     let password = $("#inputPassword").val();
 
-    if(username && password){
+    if (username && password) {
       $.ajax({
         method: "GET",
         url: url + "users/" + username,
 
-        success: (response) => {
-        //console.log(response);
-          if(response){
-          let user = response.username;
-          let pass = response.password;
-            if(username === user && password === pass){
-              window.location.href = '/homepage';
+        success: response => {
+          //console.log(response);
+          if (response) {
+            let user = response.username;
+            let pass = response.password;
+            if (username === user && password === pass) {
+              window.location.href = "/homepage";
             }
-          }
-          else{
-            $('#errLbl').html('incorrect input');
+          } else {
+            $("#errLbl").html("incorrect input");
           }
         }
-      })
+      });
     }
-  })
+  });
 
-  $(".signup").click(function(){
-    let username =
-  })
+  // $(".signup").click(function(){
+  //   let username =
+  // })
 });
-
 
 // Sign up
