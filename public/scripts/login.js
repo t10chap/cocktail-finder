3e96af492a71708be91ab366f0c02799d3c57947
t10chap <tevinrawls@gmail.com>
2018-07-19 16:10:14 -0700

    Added Error Handle Function

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -5,6 +5,25 @@ $(document).ready(function() {
     $(".formContainer").fadeIn(900);
   });
 
+  const succesfulLogin = (response) => {
+    if(response){
+    let user = response.username;
+    let pass = response.password;
+      if(username === user && password === pass){
+        window.location.href = '/homepage';
+      }
+    }
+    else{
+      $('#errLbl').html('incorrect input');
+    }
+  }
+
+  const errHandle = (err1, err2, err3) => {
+    console.log(err1);
+    console.log(err2);
+    console.log(err3);
+  }
+
 // Sign in
   $(".signin").click(function(){
     let username = $("#inputUser").val();
@@ -14,28 +33,21 @@ $(document).ready(function() {
       $.ajax({
         method: "GET",
         url: url + "users/" + username,
-
-        success: (response) => {
-        //console.log(response);
-          if(response){
-          let user = response.username;
-          let pass = response.password;
-            if(username === user && password === pass){
-              window.location.href = '/homepage';
-            }
-          }
-          else{
-            $('#errLbl').html('incorrect input');
-          }
-        }
+        success: succesfulLogin,
+        error: errHandle,
       })
     }
   })
 
-  $(".signup").click(function(){
-    let username =
-  })
-});
-
-
 // Sign up
+  $(".signup").click(function(){
+    let name = $("input[name='name']").val();
+    let username = $("input[name='username']").val();
+    let password = $("input[name='password']").val();
+    let location = $("input[name='location']").val();
+    let favLiqour = $("input[name='favLiqior']").val();
+    let favDrink = $("input[name='favDrink']").val();
+  })
+
+
+});
